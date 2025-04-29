b334c897d4a64b429b5de7b48754f6dd030e1dc6
BlankW100 <blankw100@gmail.com>
2025-04-29 18:27:41 +0800

    copoilt

--- html/assets/js/calculator.js
+++ html/assets/js/calculator.js
@@ -3,11 +3,11 @@ var date = "2023-10-01";
 var timeZone = "America/New_York";
 
 var c = moment.tz(date + " " + time, timeZone);
-console.log(c.format()) ;
+console.log(c.format());
 
 const DateTime = luxon.DateTime;
 var dt = DateTime.fromISO(c.format());
 
 dt = dt.setZone("Japan/Tokyo");
-console.log(dt.toIso(suppressMillisconds=true)) ;
-console.log(dt.toLocalString(DateTime.DATETIME_FULL)) ;
+console.log(dt.toISO({ suppressMilliseconds: true })); 
+console.log(dt.toLocaleString(DateTime.DATETIME_FULL)); 
