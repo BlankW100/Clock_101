57036a85d3d72199eee2a856625b81f673fdc13a
Tan Wei Er <weier0816@gmail.com>
2025-04-29 17:49:54 +0800

    update login system

---
+++ login&signup/login.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiOkxZFBwCP3NOXqZqpit5tF9MnwKaavQ",
+  authDomain: "clock-101-10e68.firebaseapp.com",
+  projectId: "clock-101-10e68",
+  storageBucket: "clock-101-10e68.firebasestorage.app",
+  messagingSenderId: "654434052980",
+  appId: "1:654434052980:web:d270879ef90c796a059a21",
+  measurementId: "G-VHP3DZEB3G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//inputs
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+//submit button
+const submit = document.getElementById('submit');
+submit.addEventListener("click"),function (event) {
+  event.preventDefault()
+  alert(5)
+}
