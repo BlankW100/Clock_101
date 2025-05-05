0d2fe37b7bdc48ccf41bbd96f6b0ad5772586acc
Woon <yhwoon2006@gmail.com>
2025-05-05 13:13:25 +0800

    set up user login

--- html/assets/js/signup.js
+++ html/assets/js/signup.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getDatabase, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,6 +19,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app); // Initialize auth
+const db = getDatabase(); // Initialize Firestore
+
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
 // Form submission
 const form = document.querySelector(".auth-form"); // Select the form
@@ -28,7 +34,15 @@ form.addEventListener("submit", function (event) {
   const password = document.getElementById("password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+    .then(async(userCredential) => {
+        var ref = doc(db, "users", userCredential.user.uid);
+        await setDoc(ref, {
+          email: email.value,
+          password: password.value,
+        });
+
+
+
       // Signed up
       const user = userCredential.user;
       alert("Account created successfully!");
